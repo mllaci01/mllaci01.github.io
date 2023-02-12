5689d6b33a5f70d75e81a68b7769a5260fea64c6
mllaci01 <mllaci078@gmail.com>
2023-02-12 16:53:34 +0100

    develop

--- contact.php
+++ contact.php
@@ -6,8 +6,8 @@
         $message = $_POST['message'];
 
         $mailTo = "connect@webmiller.hu";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        $headers = "MIME-Version: 1.0\r\n";
+        $headers = "Content-Type: text/html; charset=ISO-8859-1\r\n";
         $headers = "From:".$mailForm;
         $txt = "Üzenet tőle ".($name) . "\r\n".$message;
         
