c0e64d02a8b21dd2d60841e56f9cbb900f0b34a9
mllaci01 <mllaci078@gmail.com>
2023-02-12 14:54:21 +0100

    develop

--- contact.php
+++ contact.php
@@ -1,8 +1,16 @@
+$name = $_POST['name']; 
+$name = "=?UTF-8?B?".base64_encode($name )."?=";
+$targy= $_POST['targy']; 
+$targy= "=?UTF-8?B?".base64_encode($targy)."?=";
+
+
 <?php
 
     if (isset($_POST['submit'])) {
         $name = $_POST['name'];
+        $name = "=?UTF-8?B?".base64_encode($name )."?=";
         $mailForm = $_POST['mail'];
+        $mailForm= "=?UTF-8?B?".base64_encode($mailForm)."?=";
         $message = $_POST['message'];
 
         $mailTo = "connect@webmiller.hu";
